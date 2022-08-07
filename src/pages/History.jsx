94481c74d5b090c93a856f7954972f16030a1aa4
alok27a <alok27a@gmail.com>
2022-08-07 12:59:05 +0530

    Credit card fetch request added

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable array-callback-return */
 import React, { useState, useEffect } from "react";
 import { Stack, Wrap, Text, Heading } from "@chakra-ui/react";
+import { Box, Image, Badge, Button, Flex, Spacer } from "@chakra-ui/react";
 import moment from "moment";
 import TransferCard from "../components/TransferCard";
 import Sidebar from "../components/Sidebar";
@@ -26,8 +27,8 @@ const History = () => {
       }
     });
     const tes = await resp.json()
-    setUserPassword(tes.data);
-    // console.log(userpassword)
+    setCreditCard(tes.data);
+    // console.log(creditcard)
     console.log(tes)
 
   }
@@ -44,7 +45,46 @@ const History = () => {
       </Heading>
       <Stack p={4} gap={3}>
         <Wrap spacing={8}>
+          {creditcard.length === 0 ? <Text>No Saved Credit Cards Found</Text> : creditcard.map((currelem) => {
+            return (
+              <>
+                <Box w="250px" rounded="15px"
+                  overflow="hidden" bg="white" mt={10}>
+                  <Box p={5}>
+                    <Stack align="center">
+                      <Badge variant="solid" colorScheme="blue"
+                        rounded="full" px={2} fontSize='l'>
+                        {currelem.bank_name}
+                      </Badge>
+                    </Stack>
+                    <Stack align="center">
+                      <Text as="h2" fontWeight="normal" my={2} >
+                        Holder Name : {currelem.holder_name}
+                      </Text>
+                      <Text fontWeight="light">
+                        Card Number : {currelem.number}
+                      </Text>
+                      <Text fontWeight="light">
+                        Expiry Date : {currelem.expiry_date}
+                      </Text>
+                      <Text fontWeight="light">
+                        CVV : {currelem.cvv}
+                      </Text>
+                    </Stack>
+                    <Flex>
+                      <Spacer />
+                      {/* <Button variant="solid"
+                        colorScheme="blue" size="sm">
+                        Learn More
+                      </Button> */}
+                    </Flex>
+                  </Box>
+                </Box>
 
+              </>
+            )
+          })
+          }
         </Wrap>
       </Stack>
     </Sidebar>
